28713075f702bb39856c82acdb949625a23f1517
GabrielCardoso18 <126261772+GabrielCardoso18@users.noreply.github.com>
2024-09-19 22:02:28 -0300

    Alteracoes dia 19-09-2024

--- mater-play-frontend/src/App.tsx
+++ mater-play-frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import Footer from "./app/components/Footer"
 import Header from "./app/components/Header"
+import Section from "./app/components/Section"
+import HighLightSection from "./app/components/HighlightSection"
 
 function App() {
 
   return (
     <div className="wrapper">
       <Header/>
-      <main></main>
+      <main
+        style={{
+          marginTop: '8rem'
+        }}
+      >
+        <HighLightSection />
+        <Section title="Recomendados para você"/>
+        <Section title="Para toda a familia"/>
+      </main>
       <Footer/>
     </div>
 
