6baf89c26973a2a734cc1f8e803949e30ab9764f
GabrielCardoso18 <126261772+GabrielCardoso18@users.noreply.github.com>
2024-10-17 21:59:36 -0300

    Commits da aula do dia 17-10-2024

--- mater-play-frontend/src/app/components/Section/index.tsx
+++ mater-play-frontend/src/app/components/Section/index.tsx
@@ -1,26 +1,24 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
 import MovieCard from "../MovieCard";
-
-const movies = [
-    {poster: 'house-of-dragons-poster.jpg'},
-    {poster: 'gyEnhITeHLky85XJxuTPqniPrzE.jpg'},
-    {poster: '2zmTngn1tYC1AvfnrFLhxeD82hz.jpg'},
-    {poster: '7qOSKoOAPgemYhBwbJgBWcCxPWZ.jpg'},
-    {poster: '9h2KgGXSmWigNTn3kQdEFFngj9i.jpg'},
-    {poster: 'cQLJT6hTXio0md94oBsSWADOECf.jpg'},
-    {poster: 'dYwihSnQmCVuIQbuG9n18BK5Iqd.jpg'},
-    {poster: 'gyEnhITeHLky85XJxuTPqniPrzE.jpg'},
-    {poster: 'gyEnhITeHLky85XJxuTPqniPrzE.jpg'},
-    {poster: 'gyEnhITeHLky85XJxuTPqniPrzE.jpg'},
-    {poster: 'gyEnhITeHLky85XJxuTPqniPrzE.jpg'},
-
-];
+import { useEffect, useState } from "react";
+import { IMovie } from "../../@libs/types";
+import { MoviesService } from "../../services/movies-service";
 
 type SectionProps = {
     title: string;
 }
 
 function Section({title}:SectionProps) {
+    const [movies, setMovies] = useState<IMovie[]>([]); 
+
+    useEffect(() => {
+        //Executa o que está aqui dentro quando carrega o componente
+        MoviesService.getMovies()
+            .then(result => {
+               setMovies(result)
+            });
+    }, []);
+
     return (
         <Box>
             <Container>
@@ -43,7 +41,7 @@ function Section({title}:SectionProps) {
                     }}
                 >
                     {movies.map(item => (
-                       <MovieCard poster={'assets/'+item.poster}/> 
+                       <MovieCard key={item.id} poster={'assets/'+item.poster}/> 
                     )) }
 
                 </Stack>
